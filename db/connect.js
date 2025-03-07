1dcb66418c65bad280a91049e86d5956d2169bbd
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2025-03-07 20:15:31 +0600

    first commit

---
+++ db/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async (url) => {
+	try {
+		await mongoose.connect(url, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log('Database connection successful');
+	} catch (error) {
+		console.error('Database connection error:', error);
+		process.exit(1); // Exit process with failure
+	}
+};
+
+module.exports = connectDB;
+
